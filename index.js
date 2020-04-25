ab754b5311384c83d22c16edc565f24c80f2c021
n1kko777 <3734489@gmail.com>
2020-04-25 19:34:13 +0500

    WebHook

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const token = process.env.BOT_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token);
-bot.setWebHook(`${process.env.URL}/bot${token}`);
+bot.setWebHook(`${process.env.URL}/bot`);
 
 const app = new Koa();
 
